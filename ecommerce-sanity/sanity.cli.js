02cb64a182b0da26fc79349911b8498b4bf3670c
Ivan <pinjamar@gmail.com>
2023-03-18 17:00:54 +0100

    project setup and connecting sanity

---
+++ ecommerce-sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'p3d1itt5',
+    dataset: 'ecommerce'
+  }
+})
---
+++ ecommerce-sanity/sanity.config.js
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'wheat-swan',
+
+  projectId: 'p3d1itt5',
+  dataset: 'ecommerce',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
