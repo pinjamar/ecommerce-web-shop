d54a82554c2b8007b6c3efdd8f202b3e526e4927
Ivan <pinjamar@gmail.com>
2023-04-21 12:37:01 +0200

    set up layout and added navbar & footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Layout } from '../components';
 import '@/styles/globals.css';
-//import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
