1b13a64827ced9271e2ee9a3d1414ebf4487e6c6
Ivan <pinjamar@gmail.com>
2023-04-25 16:33:49 +0200

    implementing state context

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,11 +8,13 @@ import {
 
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
+import { useStateContext } from '../../';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
 
   const [index, setIndex] = useState(0);
+  const { decQty, incQty, qty } = useStateContext();
   return (
     <div>
       <div className="product-detail-container">
@@ -54,13 +56,13 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="num" onClick="">
-                0
+              <span className="num" onClick={qty}>
+                {qty}
               </span>
-              <span className="plus" onClick="">
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
